a06fd0e864352f8134aca62114358c6529378b12
Armaan Seth <99117431+ArmaanSeth@users.noreply.github.com>
2023-08-07 17:16:58 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -44,7 +44,7 @@ def split_data(df):
     y = df["Diabetic"]
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.25, random_state=0
-    )
+    ) 
 
     return X_train, X_test, y_train, y_test
     
